12a8d406b62614468d070fb623de9caeeed9455b
keyhoh <iheyhoh+github@gmail.com>
2021-08-23 13:03:12 +0900

    add: quantity

---
+++ src/main/java/keyhoh/business/type/volume/Quantity.java
@@ -0,0 +1,18 @@
+package keyhoh.business.type.volume;
+
+/**
+ * 数量
+ */
+public record Quantity(Integer value) implements Comparable<Quantity> {
+    public Quantity {
+        if (value < 0) throw new IllegalArgumentException("負値の数量は存在しません");
+    }
+    public Quantity add(final Quantity other) {
+        return new Quantity(this.value + other.value);
+    }
+
+    @Override
+    public int compareTo(final Quantity other) {
+        return this.value.compareTo(other.value);
+    }
+}
