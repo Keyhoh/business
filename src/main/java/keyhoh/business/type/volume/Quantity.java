761a8509e566c94145585b7df85e646815dfb1de
keyhoh <iheyhoh+github@gmail.com>
2021-08-23 21:02:11 +0900

    update: int to long

--- src/main/java/keyhoh/business/type/volume/Quantity.java
+++ src/main/java/keyhoh/business/type/volume/Quantity.java
@@ -1,15 +1,21 @@
 package keyhoh.business.type.volume;
 
+import java.math.BigInteger;
+
 /**
  * 数量
  */
-public record Quantity(Integer value) implements Comparable<Quantity> {
+public record Quantity(BigInteger value) implements Comparable<Quantity> {
     public Quantity {
-        if (value < 0) throw new IllegalArgumentException("負値の数量は存在しません");
+        if (value.compareTo(BigInteger.ZERO) < 0) throw new IllegalArgumentException("負値の数量は存在しません");
+    }
+
+    public Quantity(final Long value) {
+        this(BigInteger.valueOf(value));
     }
 
     public Quantity add(final Quantity other) {
-        return new Quantity(this.value + other.value);
+        return new Quantity(this.value.add(other.value));
     }
 
     @Override
--- src/test/java/keyhoh/business/type/volume/QuantityTest.java
+++ src/test/java/keyhoh/business/type/volume/QuantityTest.java
@@ -1,32 +1,32 @@
 package keyhoh.business.type.volume;
 
-import keyhoh.business.util.IntSource;
+import keyhoh.business.util.LongSource;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class QuantityTest {
-    static IntStream positiveIntStream() {
-        return IntSource.intStream().filter(i -> i >= 0);
+    static LongStream positiveLongStream() {
+        return LongSource.longStream().filter(l -> l >= 0);
     }
 
-    static IntStream negativeIntStream() {
-        return IntSource.intStream().filter(i -> i < 0);
+    static LongStream negativeLongStream() {
+        return LongSource.longStream().filter(l -> l < 0);
     }
 
     @ParameterizedTest
-    @MethodSource("positiveIntStream")
-    void constructor_does_not_throw(final int value) {
+    @MethodSource("positiveLongStream")
+    void constructor_does_not_throw(final long value) {
         assertDoesNotThrow(() -> new Quantity(value));
     }
 
     @ParameterizedTest
-    @MethodSource("negativeIntStream")
-    void constructor_throws_IllegalArgumentException(final int value) {
+    @MethodSource("negativeLongStream")
+    void constructor_throws_IllegalArgumentException(final long value) {
         assertThrows(IllegalArgumentException.class, () -> new Quantity(value));
     }
 }
