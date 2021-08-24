bb7e83d77e0ab562ea10b9032212a26fa1f7e3b5
keyhoh <iheyhoh+github@gmail.com>
2021-08-24 22:34:34 +0900

    update: インスタンスごとに年度開始月を持つように変更

--- src/main/java/keyhoh/business/time/Year.java
+++ src/main/java/keyhoh/business/time/Year.java
@@ -4,17 +4,33 @@ import java.util.Optional;
 
 /**
  * *年度
- * 年度とはビジネスにおける1年の単位です
+ * 年度とはビジネスにおける1年の単位です。
  */
-public record Year(java.time.Year value) {
-    private static java.time.Month start;
+public class Year {
+    private final java.time.Month start;
+    private final java.time.Year value;
 
-    static {
-        final Optional<String> start = Optional.ofNullable(System.getProperty("business.time.year.start"));
-        Year.start = java.time.Month.of(Integer.parseInt(start.orElse("1")));
+    /**
+     * 年度を生成します。
+     * @param start 年度開始月
+     * @param value 年
+     */
+    public Year(final java.time.Month start, final java.time.Year value) {
+        this.start = start;
+        this.value = value;
+    }
+
+    /**
+     * 年度を生成します。
+     * 年度開始月はシステムプロパティ値 `business.year.start` を使用します。
+     * 設定されていなければ年度開始月は1月です。
+     * @param value 年
+     */
+    public Year(final java.time.Year value) {
+        this(java.time.Month.of(Integer.parseInt(Optional.ofNullable(System.getProperty("business.time.year.start")).orElse("1"))), value);
     }
 
     public java.time.Year toYear(final java.time.Month month) {
-        return Year.start.compareTo(month) > 0 ? this.value.plusYears(1) : this.value;
+        return this.start.compareTo(month) > 0 ? this.value.plusYears(1) : this.value;
     }
 }
--- src/test/java/keyhoh/business/time/YearTest.java
+++ src/test/java/keyhoh/business/time/YearTest.java
@@ -9,6 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class YearTest {
     @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+    void toYear_by_default_business_year(final int month) {
+        final Year businessYear = new Year(java.time.Year.of(2000));
+        assertEquals(businessYear.toYear(Month.of(month)), java.time.Year.of(2000));
+    }
+
+    @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9, 10, 11, 12})
     void toYear_on_current_year(final int month) {
         System.setProperty("business.time.year.start", "4");
